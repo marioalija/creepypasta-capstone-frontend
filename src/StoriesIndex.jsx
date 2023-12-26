13f0490ba86d670351b190fbc03e75b8914e7164
marioalija <marioalija25@gmail.com>
2023-12-26 16:01:13 -0600

    add comments section to browser

--- src/StoriesIndex.jsx
+++ src/StoriesIndex.jsx
@@ -6,24 +6,30 @@ export function StoriesIndex(props) {
   // console.log(props);
   // const [searchFilter, setSearchFilter] = useState("");
   return (
-    <div>
+    <div className="container text-center">
       <h1>All Stories</h1>
-      {/* {props.comments.map((comment) => (
-        <div key={comment.id}>
-          <p>Comment: {comment.body}</p>
+      <div className="row">
+        <div className="col">
+          {props.stories.map((story) => (
+            <div key={story.id}>
+              <h2>{story.name}</h2>
+              <img src={story.image} />
+              <p>Descrition: {story.description}</p>
+              <p>Category: {story.category}</p>
+              <h3>Comments:</h3>
+              {story.comments.map((comment) => (
+                <div key={comment.id}>
+                  <p>
+                    {comment.user_id}: {comment.body}
+                  </p>
+                </div>
+              ))}
+              <button onClick={() => props.onShowStory(story)}>More info</button>
+              {/* <Link to={`/stories/${story.id}`}>Load Page</Link> */}
+            </div>
+          ))}
         </div>
-      ))} */}
-      {props.stories.map((story) => (
-        <div key={story.id}>
-          <h2>{story.name}</h2>
-          <img src={story.image} />
-          <p>Descrition: {story.description}</p>
-          <p>Category: {story.category}</p>
-          {/* <p>Comments: {story.comments}</p> */}
-          <button onClick={() => props.onShowStory(story)}>More info</button>
-          {/* <Link to={`/stories/${story.id}`}>Load Page</Link> */}
-        </div>
-      ))}
+      </div>
     </div>
   );
 }
